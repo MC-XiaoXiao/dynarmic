0bfad68360a1a4949a89dd0a2de892a82f28027e
MC-XiaoXiao <3194848680@qq.com>
2026-08-12 14:15:09 +0800

    jit: remove all ranges for invalidated descriptors

--- src/dynarmic/backend/block_range_information.cpp
+++ src/dynarmic/backend/block_range_information.cpp
@@ -15,11 +15,23 @@ namespace Dynarmic::Backend {
 template<typename ProgramCounterType>
 void BlockRangeInformation<ProgramCounterType>::AddRange(boost::icl::discrete_interval<ProgramCounterType> range, IR::LocationDescriptor location) {
     block_ranges.add(std::make_pair(range, std::set<IR::LocationDescriptor>{location}));
+
+    auto& descriptor_ranges = ranges_by_descriptor[location];
+    const auto previous_range_count = descriptor_ranges.iterative_size();
+    descriptor_ranges.add(range);
+    const auto current_range_count = descriptor_ranges.iterative_size();
+    if (current_range_count >= previous_range_count) {
+        range_count += current_range_count - previous_range_count;
+    } else {
+        range_count -= previous_range_count - current_range_count;
+    }
 }
 
 template<typename ProgramCounterType>
 void BlockRangeInformation<ProgramCounterType>::ClearCache() {
     block_ranges.clear();
+    ranges_by_descriptor.clear();
+    range_count = 0;
 }
 
 template<typename ProgramCounterType>
@@ -33,8 +45,34 @@ tsl::robin_set<IR::LocationDescriptor> BlockRangeInformation<ProgramCounterType>
             }
         }
     }
-    // TODO: EFFICIENCY: Remove ranges that are to be erased.
+
+    // A descriptor is invalidated as a unit. Removing only the intersection
+    // would leave its other old ranges discoverable by a later invalidation.
+    // The reverse index keeps this cleanup proportional to the invalidated
+    // descriptors and their recorded ranges instead of the whole map.
+    invalidated_descriptors += erase_locations.size();
+    for (const auto& descriptor : erase_locations) {
+        const auto descriptor_it = ranges_by_descriptor.find(descriptor);
+        if (descriptor_it == ranges_by_descriptor.end()) {
+            continue;
+        }
+
+        for (const auto& descriptor_range : descriptor_it->second) {
+            block_ranges.subtract(std::make_pair(
+                descriptor_range,
+                std::set<IR::LocationDescriptor>{descriptor}));
+        }
+        range_count -= descriptor_it->second.iterative_size();
+        ranges_by_descriptor.erase(descriptor_it);
+    }
     return erase_locations;
+}
+
+template<typename ProgramCounterType>
+typename BlockRangeInformation<ProgramCounterType>::Stats
+BlockRangeInformation<ProgramCounterType>::GetStats() const noexcept {
+    return Stats{range_count, ranges_by_descriptor.size(),
+                 invalidated_descriptors};
 }
 
 template class BlockRangeInformation<u32>;
--- src/dynarmic/backend/block_range_information.h
+++ src/dynarmic/backend/block_range_information.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
+#include <map>
 #include <set>
 
 #include <boost/icl/interval_map.hpp>
@@ -18,12 +21,23 @@ namespace Dynarmic::Backend {
 template<typename ProgramCounterType>
 class BlockRangeInformation {
 public:
+    struct Stats {
+        std::size_t range_count{};
+        std::size_t descriptor_count{};
+        std::uint64_t invalidated_descriptors{};
+    };
+
     void AddRange(boost::icl::discrete_interval<ProgramCounterType> range, IR::LocationDescriptor location);
     void ClearCache();
     tsl::robin_set<IR::LocationDescriptor> InvalidateRanges(const boost::icl::interval_set<ProgramCounterType>& ranges);
+    [[nodiscard]] Stats GetStats() const noexcept;
 
 private:
     boost::icl::interval_map<ProgramCounterType, std::set<IR::LocationDescriptor>> block_ranges;
+    std::map<IR::LocationDescriptor, boost::icl::interval_set<ProgramCounterType>>
+        ranges_by_descriptor;
+    std::size_t range_count{};
+    std::uint64_t invalidated_descriptors{};
 };
 
 }  // namespace Dynarmic::Backend
